0b7e6c147a2bf7eb51f6c4b668ba61cd99f44539
JH Paul <paulusg131@gmail.com>
2025-05-06 18:46:55 +0200

    SYNTAX_UPDATE n2z3i5k3

--- src/mem/perfomanceOptimization.js
+++ src/mem/perfomanceOptimization.js
@@ -1,6 +1,5 @@
 const { app } = require('../index');
 
-// Optimize App runtime performance.
 app.on('ready', () => {
     app.commandLine.appendSwitch('disable-http-cache');
     app.commandLine.appendSwitch('enable-low-end-device-mode');
