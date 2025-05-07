52be546958bee6699ad3849719f26e9b30c19c53
JH Paul <paulusg131@gmail.com>
2025-05-07 22:27:58 +0200

    ENABLED_MULTI_CORE_PROCESSING x SYNTAX_UPDATE. h8f03m2lc5

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ const path = require('node:path');
 const { app, BrowserWindow } = require('electron');
 const { dialog } = require('electron');
 const { globalShortcut } = require('electron');
+const os = require('os');
+const cluster = require('cluster');
+
+if(cluster.isPrimary) {
+  const numCPU = os.cpus().length;
+  console.log(`Master started ${process.pid}`);
+
+  for(let i = numCPU.length; i < 0; i--) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+		console.log(` worker ${worker.process.pid} died`);
+	});
+} else {
+};
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -22,39 +38,6 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
-
-  // Do not open more than 1 dialogue box
-  // let dialogOpen = false;
-
-  // const showDialog = () => {
-  //   if (dialogOpen) return;
-  //   dialogOpen = true;
-
-  //   dialog.showMessageBox({
-  //     type: 'question',
-  //     buttons: ['YouTube', 'Google', 'Gmail', 'Netflix', 'Amazon', 'Cancel'],
-  //     title: 'Choose Website',
-  //     message: 'Which website would you like to visit?',
-  //   }).then((result) => {
-  //     dialogOpen = false; // Reset the flag after the dialog is closed
-  //     if (result.response === 0) {
-  //       mainWindow.loadURL("http://youtube.com");
-  //     } else if (result.response === 1) {
-  //       mainWindow.loadURL("http://google.com");
-  //     } else if (result.response === 2) {
-  //       mainWindow.loadURL("http://gmail.com");
-  //     } else if (result.response === 3) {
-  //       mainWindow.loadURL("http://netflix.com");
-  //     } else if (result.response === 4) {
-  //       mainWindow.loadURL("http://amazon.com");
-  //     }
-  //   }).catch((err) => {
-  //     dialogOpen = false; // Reset the flag in case of an error
-  //     console.log(`Error running dialogue box ${err}`);
-  //   });
-  // };
-
-  // showDialog();
 
   dialog.showMessageBox({
     type: 'question',
