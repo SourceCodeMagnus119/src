2a5ee63975fd9b3b5a44382a19fb1ebf35d6eeef
JH Paul <paulusg131@gmail.com>
2025-05-09 01:34:18 +0200

    NOTIFICATION_SYSTEM_ x RENDERER_NOTIFICATION_PROCESS. f0m3f8c03jx8

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const NOTIFICATION_TITLE = 'MasterChief 119';
+const NOTIFICATION_BODY = 'Where is my Weapon';
+const CLICK_MESSAGE = 'All Hail MasterChief119, His power is over 9000!';
+
+// new window.Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
+// .onclick = () => { document.getElementById('output').innerText = CLICK_MESSAGE };
