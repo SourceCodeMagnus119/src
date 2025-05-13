4dab1a934349087f4503fc489375908a21024997
JH Paul <paulusg131@gmail.com>
2025-05-13 19:30:48 +0200

    IPC_RENDERING_MAIN_WINDOW_LOGIC x SYNTAX_UPDATE. k2d9h5d83n

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,6 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { ipcRender } = require("electron");
 
 const NOTIFICATION_TITLE = 'MasterChief 119';
 const NOTIFICATION_BODY = 'Where is my Weapon';
