11e773bbc6b493c5a40375f8ad6f18d7c86ba702
JH Paul <paulusg131@gmail.com>
2025-05-15 17:09:02 +0200

    FUSE_CONFIGURATION x FUSE_RUN_AS_NODE x FUSE_ENABLE_NODE_OPTIONS_ENVIRONMENT_VARIABLES x FUSE_ENABLE_NODE_CLI_INSPECTION_ARGUMENTS. l2k3if83n5

--- forge.config.js
+++ forge.config.js
@@ -4,12 +4,15 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './assets/syff-app-icon.favicon.ico',
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        setupIcon: './assets/syff-app-icon.favicon.ico',
+      },
     },
     {
       name: '@electron-forge/maker-zip',
@@ -33,10 +36,10 @@ module.exports = {
     // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.RunAsNode]: true,
       [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: true,
+      [FuseV1Options.EnableNodeCliInspectArguments]: true,
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
