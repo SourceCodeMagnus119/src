86155ee64ea1d0c37ed378c8bd237bf613388eaf
JH Paul <paulusg131@gmail.com>
2025-05-27 15:38:32 +0200

    LOCAL_PACKAGE_INSTALLER x WINDOWS_MAC_LINUX. u48n3d-25

--- src/installer/installer.js
+++ src/installer/installer.js
@@ -1,9 +1,31 @@
-const packager = require("@electron/packager");
-const osx_Sign = require("@electron/osx-sign");
-const desktopinstaller = require('electron-winstaller');
+// const packager = require("@electron/packager");
+// const osx_Sign = require("@electron/osx-sign");
+// const desktopinstaller = require('electron-winstaller');
 
-const windowsInstaller = () => {
-    desktopinstaller.createWindowsInstaller(() => {});
-};
+// const windowsInstaller = () => {
+//     desktopinstaller.createWindowsInstaller({
+//         appDirectory: './dist/win-unpacked',
+//         outputDirectory: './dist/installer',
+//         authors: 'Verrsoft Inc',
+//         exe: 'syff-app-v1.190.exe'
+//     })
+//     .then(() => {
+//         return osx_Sign({
+//             app: './dist/mac/syff-app.app',
+//             identity: 'Developer ID Application: Verrsoft Inc (YOUR_TEAM_ID)',
+//             hardenedRuntime: true,
+//             entitlements: './entitlements.plist',
+//             'entitlements-inherit': './entitlements.plist'
+//         });
+//     })
+//     .then(() => {
+//         console.log('Windows installer created successfully!');
+//         console.log('macOS app signed successfully!');
+//     })
+//     .catch((err) => {
+//         console.error('Error during packaging:', err);
+//     });
+// };
+// // windowsInstaller();
 
-module.exports = { linuxInstaller };
+// module.exports = { windowsInstaller };
