8efba21d1965723dd5ee41eb8a37eeacae6ca2fb
JH Paul <paulusg131@gmail.com>
2025-05-28 21:13:27 +0200

    PROPERIES_DISABLED. j2b4d0b2s8

--- src/mem/perfomanceOptimization.js
+++ src/mem/perfomanceOptimization.js
@@ -1,7 +1,7 @@
 const { app } = require('../index');
 
 app.on('ready', () => {
-    app.commandLine.appendSwitch('disable-http-cache');
-    app.commandLine.appendSwitch('enable-low-end-device-mode');
+    // app.commandLine.appendSwitch('disable-http-cache');
+    // app.commandLine.appendSwitch('enable-low-end-device-mode');
     app.commandLine.appendSwitch('disable-background-timer-throttling');
 });
