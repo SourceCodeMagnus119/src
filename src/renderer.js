d60903238c686c9d3f7dcb92a7a2a87474614cf8
JH Paul <paulusg131@gmail.com>
2025-06-06 16:40:37 +0200

    RENDERER_PROCESS_VERSIONING_DISPLAY. kk1n4d0n2b509d

--- src/renderer.js
+++ src/renderer.js
@@ -1,2 +1,9 @@
 const information = document.getElementById('info');
 information.innerText = `This app is using chrome (v${version.chrome()})`
+
+const yinYang = async() => {
+    const response = await window.versions.ping();
+    console.log(response)
+}
+
+yinYang();
