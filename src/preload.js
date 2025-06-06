102374d558743063752076da32d6241bdee456e3
JH Paul <paulusg131@gmail.com>
2025-06-06 16:34:55 +0200

    SETUP_INTER_PROCESS_COMMUNICATION_MAIN_RENDERER_PROCESS. j1b4i2d9b5g7f9

--- src/preload.js
+++ src/preload.js
@@ -1,34 +1,39 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const { pushNotifications } = require('electron/main');
 
-contextBridge.exposeInMainWorld('electronAPI', {
-	saveLastVisitedUrl: (url) => ipcRenderer.send('save-last-visited-url', url),
-	PictureInPictureEvent: () => ipcRenderer.invoke('picture-in-picture'),
-	History: () => ipcRenderer.invoke('history'),
-
-	Cache: () => ipcRenderer.invoke('cache'),
-	CacheStorage: () => ipcRenderer.invoke('cache-storage'),
-	Notification: () => ipcRenderer.invoke('notification'),
-	pushNotifications: () => ipcRenderer.invoke('push-notifications'),
-
-	PerformanceNavigationTiming: () => ipcRenderer.invoke('performanceNavigationTiming'),
-	NavigationHistoryEntry: () => ipcRenderer.invoke('navigation-history-timing'),
-	PerformanceResourceTiming: () => ipcRenderer.invoke('performanceResourceTiming'),
+contextBridge.exposeInMainWorld('versions', {
+	node: () => process.versions.node,
+	chrome: () => process.versions.chrome,
+	electron: () => process.versions.electron,
+	ping: () => ipcRenderer.invoke('yin'),
+	// PictureInPictureEvent: () => ipcRenderer.invoke('picture-in-picture'),
 	
-	getLastVisitedUrl: () => ipcRenderer.invoke('get-last-visited-url')
-	,  
-		openLastVisitedUrl: async () => {
-			const url = await ipcRenderer.invoke('get-last-visited-url');
-			if (url) {
-				window.location.href = url;
-			}
-		}	
+	// saveLastVisitedUrl: (url) => ipcRenderer.send('save-last-visited-url', url),
+	// History: () => ipcRenderer.invoke('history'),
+
+	// Cache: () => ipcRenderer.invoke('cache'),
+	// CacheStorage: () => ipcRenderer.invoke('cache-storage'),
+	// Notification: () => ipcRenderer.invoke('notification'),
+	// pushNotifications: () => ipcRenderer.invoke('push-notifications'),
+
+	// PerformanceNavigationTiming: () => ipcRenderer.invoke('performanceNavigationTiming'),
+	// NavigationHistoryEntry: () => ipcRenderer.invoke('navigation-history-timing'),
+	// PerformanceResourceTiming: () => ipcRenderer.invoke('performanceResourceTiming'),
+	
+	// getLastVisitedUrl: () => ipcRenderer.invoke('get-last-visited-url')
+	// ,  
+	// 	openLastVisitedUrl: async () => {
+	// 		const url = await ipcRenderer.invoke('get-last-visited-url');
+	// 		if (url) {
+	// 			window.location.href = url;
+	// 		}
+	// 	}
 });
 
-ipcRenderer.on('set-session-storage', (event, { key, value }) => {
-	window.sessionStorage.setItem(key, value);
-});
+// ipcRenderer.on('set-session-storage', (event, { key, value }) => {
+// 	window.sessionStorage.setItem(key, value);
+// });
 
-ipcRenderer.on('clear-session-storage', () => {
-	window.sessionStorage.clear();
-});
+// ipcRenderer.on('clear-session-storage', () => {
+// 	window.sessionStorage.clear();
+// });
