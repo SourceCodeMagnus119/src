5c476f60cf725af4b7fed57d5f2a3a4e40f830e6
JH Paul <paulusg131@gmail.com>
2025-06-13 18:13:54 +0200

    IPC_PICTURE_IN_PICTURE x IPC_SESSION_SYSTEM x IPC_SESSION_STORAGE. pp1a8s5b3c0n4

--- src/preload.js
+++ src/preload.js
@@ -6,9 +6,9 @@ contextBridge.exposeInMainWorld('versions', {
 	chrome: () => process.versions.chrome,
 	electron: () => process.versions.electron,
 	ping: () => ipcRenderer.invoke('yin'),
-	// PictureInPictureEvent: () => ipcRenderer.invoke('picture-in-picture'),
-	// sesion: () => ipcRenderer.invoke('sesion');
-	// sessionStorage: () => ipcRenderer.send('set-session-storage');
+	PictureInPictureEvent: () => ipcRenderer.invoke('picture-in-picture'),
+	sesion: () => ipcRenderer.invoke('sesion'),
+	sessionStorage: () => ipcRenderer.send('set-session-storage'),
 	// Cache: () => ipcRenderer.invoke('cache'),
 	// CacheStorage: () => ipcRenderer.invoke('cache-storage'),
 	// pushNotifications: () => ipcRenderer.invoke('push-notifications'),
