4306394b6a69adf0217bc089db94270ac628089b
JH Paul <paulusg131@gmail.com>
2025-06-25 18:14:58 +0200

    SYNTAX_UPDATE_RENDERER_PROCESS. i5s0nl5jf9b3d9

--- src/renderer.js
+++ src/renderer.js
@@ -2,10 +2,8 @@ const information = document.getElementById('info');
 information.innerText = `This app is using chrome (v${version.chrome()})`
 // console.log(information);
 
-// Send a test message to the main process from the renderer process.
 const yinYang = async() => {
     const response = await window.versions.ping();
     console.log(response)
 }
-
 yinYang();
