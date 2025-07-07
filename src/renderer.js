c71faeb7e61e4effeb83cb5909a33ed71c9eb77a
JH Paul <paulusg131@gmail.com>
2025-07-07 22:49:30 +0200

    RENDERER_LOGIC_UPDATE x SYNTAX_UPDATE. u49wb2a0n4o28fb37b29i

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,9 @@
-const information = document.getElementById('info');
-information.innerText = `This app is using chrome (v${version.chrome()})`
+// const information = document.getElementById('info');
+// information.innerText = `This app is using chrome (v${version.chrome()})`
 // console.log(information);
 
 const yinYang = async() => {
     const response = await window.versions.ping();
-    console.log(response)
+    console.log(`This is the result of the two-way IPC massage from the renderer process: ${response}`)
 }
 yinYang();
