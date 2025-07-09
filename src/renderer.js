efb4bc8ace809ffb551c9e57aaecb00dca09575e
JH Paul <paulusg131@gmail.com>
2025-07-10 00:35:30 +0200

    SYNTAX_UPDATE. k108w9b2d8b429ai0n2d8

--- src/renderer.js
+++ src/renderer.js
@@ -2,7 +2,7 @@
 // information.innerText = `This app is using chrome (v${version.chrome()})`
 // console.log(information);
 
-const yinYang = async() => {
+async function yinYang() {
     const response = await window.versions.ping();
     console.log(`This is the result of the two-way IPC massage from the renderer process: ${response}`)
 }
