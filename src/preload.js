d8242fdeb3e84926522205520b6bb65aea8083bb
JH Paul <paulusg131@gmail.com>
2025-07-23 16:27:08 +0200

    SYNTAX_UPDATE_APP_API_TITLE. h93g9dh392uv2nx0n4x0c7

--- src/preload.js
+++ src/preload.js
@@ -1,14 +1,16 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const { pushNotifications } = require('electron/main');
 
-contextBridge.exposeInMainWorld('versions', {
-	node: () => process.versions.node,
-	chrome: () => process.versions.chrome,
-	electron: () => process.versions.electron,
+contextBridge.exposeInMainWorld('SyffAPI', {
 	ping: () => ipcRenderer.invoke('yin'),
+	electron: () => process.versions.electron,
+	chrome: () => process.versions.chrome,
+	node: () => process.versions.node,
+
 	PictureInPictureEvent: () => ipcRenderer.invoke('picture-in-picture'),
 	sesion: () => ipcRenderer.invoke('sesion'),
 	sessionStorage: () => ipcRenderer.invoke('set-session-storage'),
+
 	Cache: () => ipcRenderer.invoke('cache'),
 	CacheStorage: () => ipcRenderer.invoke('cache-storage'),
 	History: () => ipcRenderer.invoke('history'),
