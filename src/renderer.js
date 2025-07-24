6276aead898eb7aad06ff4e704543de1f96d3744
JH Paul <paulusg131@gmail.com>
2025-07-24 20:24:42 +0200

    RENDERER_PROCESS_IP_LOCK_DISPLAY_TEST. g9t1f8fn30d4x9e5v94n38sr9

--- src/renderer.js
+++ src/renderer.js
@@ -2,8 +2,14 @@
 // information.innerText = `This app is using chrome (v${version.chrome()})`
 // console.log(information);
 
+const ipLock = async() => {
+    const ipLock = await window.SyffAPI.IpLock()
+    console.log(ipLock)
+}
+ipLock()
+
 async function yinYang() {
-    const response = await window.versions.ping();
+    const response = await window.SyffAPI.ping();
     console.log(`This is the result of the two-way IPC massage from the renderer process: ${response}`)
 }
 yinYang();
