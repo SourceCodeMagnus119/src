2986ab3b10fe2f064bdc7760522116a6cf76a57a
JH Paul <paulusg131@gmail.com>
2025-08-02 21:44:45 +0200

    PERFORMANCE_METRICS x REMOTE_HOST_RESOLVER x SESSION_LIFETIME_USER_AGENT x UPDATE_CURRENT_ACTIVITY. g02nd94bls0f92bf8g03n0

--- src/mem/perfomanceOptimization.js
+++ src/mem/perfomanceOptimization.js
@@ -5,4 +5,12 @@ app.on('ready', () => {
     // app.commandLine.appendSwitch('disable-http-cache');
     // app.commandLine.appendSwitch('enable-low-end-device-mode');
     app.commandLine.appendSwitch('disable-background-timer-throttling');
+
+    app.addRecentDocument()
+    app.applicationMenu()
+    app.configureHostResolver()
+    app.getGPUInfo("basic")
+    app.getAppMetrics(true)
+    app.updateCurrentActivity()
+    app.userAgentFallback()
 });
