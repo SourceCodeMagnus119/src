aa69ca51d3d4c2d17d23b6584f75f1c059ded272
JH Paul <paulusg131@gmail.com>
2025-08-07 15:16:12 +0200

    SYNTAX_UPDATE. t09n2a0gn3l2

--- src/mem/perfomanceOptimization.js
+++ src/mem/perfomanceOptimization.js
@@ -5,7 +5,7 @@ app.on('ready', () => {
     // app.commandLine.appendSwitch('disable-http-cache');
     // app.commandLine.appendSwitch('enable-low-end-device-mode');
     app.commandLine.appendSwitch('disable-background-timer-throttling');
-
+    
     app.addRecentDocument("C:/Users/Untoasted_Raisin/Desktop")
     app.applicationMenu()
     app.configureHostResolver()
